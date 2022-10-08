1e7dd4be2592df79b2ad571418835d4a9af7a3da
Reshma <reshma.dhillon@kcl.ac.uk>
2022-10-08 15:24:52 +0100

    Adding ValueError exception if number_of_primes <=0

--- primes.py
+++ primes.py
@@ -2,5 +2,18 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError(f"Number of primes = {number_of_primes}. Should have been an integer greater than 0.")
     list = []
+    i = 2
+    list.append(i)
+    while len(list) < number_of_primes:
+        prime = True
+        i = i + 1
+        for j in range (2,i):
+            if i % j == 0:
+                prime = False
+                break
+        if prime == True:
+            list.append(i)
     return list
